d33c95749d28b4a43b17a06baa54e7ab3a7db1f1
RODRIGO PALOMO REY <145378343+rodrigopalomo21@users.noreply.github.com>
2023-10-11 15:00:55 +0000

    defs

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def index(request):
     context = {
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+
+#dame una serie de paginas que se llamen 
+
+def home(request):
+    return HttpResponse("Inicio")
+
+def about(request):
+    return HttpResponse("Historia")
+
+def services(request):
+    return HttpResponse("Servicios")
+
+def store(request):
+    return HttpResponse("Visítanos")
+
+def contact(request):
+    return HttpResponse("Contacto")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def sample(request):
+    return HttpResponse("Sample")
+core/urls.py
+from django.contrib import admin
+from django.urls import path
+from core import views
